ef14cdfad747fffe67fef75ae07421809d676d59
Dallin Bentley <Dallyb9@gmail.com>
2022-10-01 13:08:21 -0600

    higher lower

--- HigherOrLower/MakeDeck.cs
+++ HigherOrLower/MakeDeck.cs
@@ -1,3 +1,28 @@
-class Deck {
 
+class Deck {
+    // Make four suits with 13 cards each
+    List<string> hearts = new list<string>(){
+
+    };
+    list<string> diamonds = new list<string>(){
+
+    };
+     List<string> clubs = new list<string>(){
+
+    };
+     List<string> spades = new list<string>(){
+
+    };
+
+    // Make a function to randomly pick a suit and then randomly pick
+    // a card within the suit, then remove that card from the list
+
+    // Make a higher or lower guessing function for the player
+    // And add/ subtract points when they get it right or wrong
+
+
+    // Make a win condition when the player goes through all the cards
+    // or when they get a certain amount of points
+
+    // Make a lose conditinon when the player loses all their points
 }
