7aed83c8a53a9159dca21dd50978e5846a071858
Brycen Taylor <bkt443@byui.edu>
2022-10-04 14:19:24 -0600

    Added to program.cs

--- HigherOrLower/Program.cs
+++ HigherOrLower/Program.cs
@@ -1,4 +1,21 @@
 ﻿class Program
 {
+    static void Main(string[] args)
+    {
+        Deck deck = new Deck();
+
+        while (!IsGameOver(deck))
+        {
+            board.print();
+
+            int choice = GetMoveChoice(currentPlayer);
+            MakeMove(board, choice, currentPlayer);
+
+            currentPlayer = GetNextPlayer(currentPlayer);
+        }
+
+        board.print();
+        Console.WriteLine("Good game. Thanks for playing!");
+    }
 
 }
