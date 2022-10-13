a43ed08899086488c3d853fa54e9975f129e3923
Brycen Taylor <bkt443@byui.edu>
2022-10-13 15:59:25 -0600

    made crap

--- Jumper/Word.cs
+++ Jumper/Word.cs
@@ -1,4 +1,12 @@
-class Word{
-    
+class Word
+{
+    string[] words = { "eight", "whale", "Obama", "Stink", "Onion", "Snake", "Train", "Fries" };
+
+    public string chooseword()
+    {
+        Random random = new Random();
+        int wordindex = random.Next(0, words.Length);
+        return words[wordindex];
+    }
 
 }
