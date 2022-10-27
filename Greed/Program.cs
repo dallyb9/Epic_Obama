d1d56471e0c0744a9a125076adfd0574b0431d86
Dallin Bentley <Dallyb9@gmail.com>
2022-10-27 15:02:54 -0600

    greed update

--- Greed/Program.cs
+++ Greed/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+using System.Numerics;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        public static void Main()
+        {
+
+            var ScreenHeight = 400;
+            var ScreenWidth = 800;
+            var BallPosition = new Vector2(ScreenHeight / 2, ScreenWidth / 2);
+            var BallMovementSpeed = 10;
+            float BallRadius = 50;
+
+            Raylib.InitWindow(ScreenHeight, ScreenWidth, "Ball");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Move the ball with the arrow keys!", 12, 12, 20, Color.BLACK);
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+                    BallPosition.X += BallMovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+                    BallPosition.X -= BallMovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP)) {
+                    BallPosition.Y -= BallMovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN)) {
+                    BallPosition.Y  += BallMovementSpeed;
+                }
+                if (Raylib BallPosition>= ScreenHeight){
+                    
+                }
+
+                Raylib.DrawCircleV(BallPosition, BallRadius, Color.MAROON);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
