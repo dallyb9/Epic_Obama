596088090fbbdcb716f26865d1a303c1e179b8a9
Brycen Taylor <bkt443@byui.edu>
2022-11-03 15:04:02 -0600

    Update Fallingobject.cs

--- Greed/Fallingobject.cs
+++ Greed/Fallingobject.cs
@@ -1,5 +1,10 @@
 class Fallingobject{
-    int movedown(object){
-        return object.Y++;
+    public Vector2 Position { get; set; } = new Vector2(0, 0);
+    public Vector2 Velocity { get; set; } = new Vector2(0, 0);
+    public void movedown(){
+        Vector2 NewPosition = Position;
+        NewPosition.X += Velocity.X;
+        NewPosition.Y += Velocity.Y;
+        Position = NewPosition;
     }
 }
