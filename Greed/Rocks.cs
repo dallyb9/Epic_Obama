ccdb1bdfd4aa00a792b7bdc015b873b89d1c3940
Brycen Taylor <bkt443@byui.edu>
2022-11-07 10:27:48 -0700

    ye

--- Greed/Rocks.cs
+++ Greed/Rocks.cs
@@ -4,8 +4,8 @@ class Rocks : Fallingobject
 {
     string rock_template = "[]";
     var RockSize = 50;
-    override public void Draw()
+    public void Draw()
     {
-        Raylib.DrawRectangle((int)Position.X, (int)Position.Y, RockSize, RockSize, Color);
+        Raylib.DrawRectangle((int)Position.X, (int)Position.Y, RockSize, RockSize, Color.BLUE);
     }
 }
